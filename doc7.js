614885a94022dd4a2fd9497699b22cfeb04eafa9
Nanipatel33 <144881647+Nanipatel33@users.noreply.github.com>
2024-02-17 17:47:54 +0530

    Add files via upload

---
+++ doc7.js
@@ -0,0 +1,25 @@
+
+        function red(){
+            let raj=document.getElementById("bt1").style.backgroundColor="#cf1124"
+            let raj2=document.getElementById("r1").style.backgroundColor="#cf1124"
+            let raj3=document.getElementById("bt3").style.backgroundColor="#1f1d41"
+            let raj4=document.getElementById("r3").style.backgroundColor="#4b5069"
+            let raj5=document.getElementById("bt2").style.backgroundColor="#1f1d41"
+            let raj6=document.getElementById("r2").style.backgroundColor="#4b5069"
+        }
+        function yellow(){
+            let raj=document.getElementById("bt2").style.backgroundColor="#f7c948"
+            let raj2=document.getElementById("r2").style.backgroundColor="#f7c948"
+            let raj3=document.getElementById("bt1").style.backgroundColor="#1f1d41"
+            let raj4=document.getElementById("r1").style.backgroundColor="#4b5069"
+            let raj5=document.getElementById("bt3").style.backgroundColor="#1f1d41"
+            let raj6=document.getElementById("r3").style.backgroundColor="#4b5069"
+        }
+        function green(){
+            let raj=document.getElementById("bt3").style.backgroundColor="#199473"
+            let raj2=document.getElementById("r3").style.backgroundColor="#199473"
+            let raj3=document.getElementById("bt2").style.backgroundColor="#1f1d41"
+            let raj4=document.getElementById("r2").style.backgroundColor="#4b5069"
+            let raj5=document.getElementById("bt1").style.backgroundColor="#1f1d41"
+            let raj6=document.getElementById("r1").style.backgroundColor="#4b5069"
+        }
